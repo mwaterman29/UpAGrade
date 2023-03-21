2d67b3d122b4a92f51a859f9d3214c5f18db0b2c
ascribner2 <aidan_scribner@student.uml.edu>
2023-03-20 21:26:28 -0400

    I need to pull real quick

--- app/addclimb.tsx
+++ app/addclimb.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 import { Link } from "expo-router";
 
 export default function TrackClimbs() {
 
     return (
         <View className="flex-1 items-center justify-center bg-purple-100">
-            <Text>This will be the add screen</Text>
-            <StatusBar />
+            <Text className='text-center text-4xl font-bold mt-16 mb-3'>Add Climb</Text>
+            
+            
+            <TextInput />
+            <TextInput />
+
         </View>
     );
 }
--- app/viewClimb.tsx
+++ app/viewClimb.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, Button, ScrollView } from 'react-native';
 import { Link } from "expo-router";
-import InjuryCard from './components/InjuryCard';
+
 
 export default function ViewClimb() {
     return (
         <View className="flex-1 h-full bg-ug-white">
             <Text className='text-center text-4xl font-bold mt-16 mb-3'>View Climb</Text>
+            <ScrollView>
+
+                
+            </ScrollView>
         </View>
     );
 }

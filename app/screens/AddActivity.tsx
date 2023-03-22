2a4b164f5447c35059c1b28d5d2100f1c8745614
Matt <mwaterman29@gmail.com>
2023-03-22 18:48:59 -0400

    layouting / selection improved

--- app/screens/AddActivity.tsx
+++ app/screens/AddActivity.tsx
@@ -1,8 +1,9 @@
 import ScreenLayout from "../components/ScreenLayout";
 import React, {ReactNode, useState} from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, TextInput} from 'react-native';
 import RadioGroup, {RadioButtonProps} from 'react-native-radio-buttons-group';
 import { Link } from "expo-router";
+
 
 const AddActivity = () => {
 
@@ -10,19 +11,31 @@ const AddActivity = () => {
         {
             id: '1', // acts as primary key, should be unique and non-empty string
             label: '',
-            value: 'option1',
-            size:25
+            value: '#X#D',
+            size:35,
+            selected:true
         },
         {
             id: '2',
             label: '',
-            value: 'option2'
+            value: '#D',
+            size:35
+        },
+        {
+            id: '3',
+            label: '',
+            value: 'D',
+            size:35
         }
     ]);
 
     function onPressRadioButton(radioButtonsArray: RadioButtonProps[]) {
-        setRadioButtons(radioButtonsArray);
+        setRadioButtons(radioButtonsArray);  
     }
+
+    const [num, onChangeNum] = React.useState('');
+    const [by, onChangeBy] = React.useState('');
+    const [desc, onChangeDesc] = React.useState('');
 
     return(
         <ScreenLayout>
@@ -34,25 +47,77 @@ const AddActivity = () => {
                     Pick Type
                 </Text>
                 
-                <View className="flex flex-row w-full bg-ug-brown ">
+                <View className="flex flex-row w-full">
                     <View className="flex flex-col basis-4/5">
-                        <View className="flex-row items-center bg-ug-gray h-24 min-h-24 w-full">
+                        <View className="flex-row items-center bg-ug-gray my-2 h-24 min-h-24 w-full">
                             <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
                             <Text className="flex basis-1/6 text-center items-center justify-center h-full">X</Text>
                             <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
-                            <Text className="flex basis-1/2 bg-ug-brown text-center items-center justify-center h-full">Test Description</Text>
+                            <Text className="flex basis-1/2 bg-ug-light-blue text-center items-center justify-center h-full">Test Description</Text>
+                        </View>
+                        <View className="flex-row items-center bg-ug-gray my-2 h-24 min-h-24 w-full">
+                            <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
+                            <Text className="flex basis-1/6 text-center items-center justify-center h-full">X</Text>
+                            <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
+                            <Text className="flex basis-1/2 bg-ug-light-blue text-center items-center justify-center h-full">Test Description</Text>
+                        </View>
+                        <View className="flex-row items-center bg-ug-gray my-2 h-24 min-h-24 w-full">
+                            <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
+                            <Text className="flex basis-1/6 text-center items-center justify-center h-full">X</Text>
+                            <Text className="flex basis-1/6 bg-ug-light-green text-center items-center justify-center h-full">#</Text>
+                            <Text className="flex basis-1/2 bg-ug-light-blue text-center items-center justify-center h-full">Test Description</Text>
                         </View>
                     </View>
-                    <View className="flex basis-1/5">
+                    <View className="py-6 flex h-full basis-1/5">
                         <RadioGroup 
                             radioButtons={radioButtons} 
                             onPress={onPressRadioButton} 
+                            containerStyle={{ justifyContent: 'space-between', height: 240}}
                         />
                     </View>
                 </View>
                 <Text className="w-full text-2xl text-ug-light-green text-center">
                     Describe Activity
                 </Text>
+                <View className="flex w-full">
+                    {radioButtons.find(rb => rb.value === '#X#D')?.selected && (
+                    <View className="flex grow w-full bg-ug-gray">
+                        <TextInput 
+                            className="flex w-full bg-ug-light-green text-center"
+                            onChangeText={onChangeDesc}
+                            value={desc}
+                            placeholder="# of Reps"
+                        />
+                        <TextInput 
+                            className="flex w-full bg-ug-light-green text-center"
+                            onChangeText={onChangeDesc}
+                            value={desc}
+                            placeholder="By ..."
+                        />
+                        <TextInput 
+                            className="flex w-full bg-ug-light-blue text-center"
+                            onChangeText={onChangeDesc}
+                            value={desc}
+                            placeholder="Description"
+                        />
+                    </View>
+                    )}
+                    {radioButtons.find(rb => rb.value === '#D')?.selected && (
+                        <View>
+                            <Text>Test option 2</Text>
+                        </View>
+                    )}
+                    {radioButtons.find(rb => rb.value === 'D')?.selected && (
+                        <View className="flex w-full bg-ug-gray">
+                            <TextInput 
+                                className="flex w-full bg-ug-light-blue text-center"
+                                onChangeText={onChangeDesc}
+                                value={desc}
+                                placeholder="Description"
+                            />
+                        </View>
+                    )}
+                </View>
             </View>
         </ScreenLayout>
     );

dfd6f26e34f4726f0e8694f2602352916a7d896e
Matt <mwaterman29@gmail.com>
2023-04-12 15:38:23 -0400

    bg on Str.Asess

--- app/components/ScreenLayout.tsx
+++ app/components/ScreenLayout.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect, ReactNode} from 'react';
-import { Text, View, KeyboardAvoidingView, Keyboard} from 'react-native';
+import { Text, View, KeyboardAvoidingView, Keyboard, ImageBackground} from 'react-native';
 import { Link } from "expo-router";
 
 //Component imports
@@ -8,9 +8,10 @@ import Navbar from './Navbar';
 
 type Props = {
     children?: ReactNode
+    img?: any
 }
   
-const ScreenLayout = ({children}: Props) => {
+const ScreenLayout = ({children, img}: Props) => {
 
     const [isKeyboardVisible, setKeyboardVisible] = useState(false);
     useEffect(() => {
@@ -33,21 +34,48 @@ const ScreenLayout = ({children}: Props) => {
         };
       }, []);
 
-    return(
-        <KeyboardAvoidingView>
-                <View className="flex flex-col w-full h-full bg-ug-light-gray">
-                    <View className='flex basis-[90%] bg-ug-light-gray p-4'>
-                        {children}
-                    </View>       
-                    {
-                    !isKeyboardVisible &&
-                    <View className='flex basis-[10%] bg-ug-light-gray p-2'>
-                    <Navbar/>
-                    </View>  
-                    }
-                </View>
-        </KeyboardAvoidingView>
-    )
+
+      if(img)
+      {
+        return(
+          <KeyboardAvoidingView>
+            <ImageBackground source={img} className='h-full w-full'>
+              <View className='bg-ug-white opacity-50 absolute bottom-0 left-0 w-full h-full'>
+              </View>
+              <View className="flex flex-col w-full h-full">
+                  <View className='flex basis-[90%] p-4'>
+                      {children}
+                  </View>       
+                  {
+                  !isKeyboardVisible &&
+                  <View className='flex basis-[10%] bg-ug-light-gray p-2'>
+                  <Navbar/>
+                  </View>  
+                  }
+              </View>
+            </ImageBackground>
+          </KeyboardAvoidingView>
+        )
+      }
+      else
+      {
+        return(
+          <KeyboardAvoidingView>
+              <View className="flex flex-col w-full h-full bg-ug-light-gray">
+                  <View className='flex basis-[90%] bg-ug-light-gray p-4'>
+                      {children}
+                  </View>       
+                  {
+                  !isKeyboardVisible &&
+                  <View className='flex basis-[10%] bg-ug-light-gray p-2'>
+                  <Navbar/>
+                  </View>  
+                  }
+              </View>
+          </KeyboardAvoidingView>
+      )
+      }
+
 }
 
 export default ScreenLayout
